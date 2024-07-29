90cdd03de124925c60977aaa05224b8ed9a521ba
Mutai001 <omusekim@gmail.com>
2024-07-30 00:28:11 +0300

    components and styles

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'brand-blue': '#007bff',
+                'brand-darkblue': '#003366',
+            },
+        },
+    },
+    plugins: [],
+};
